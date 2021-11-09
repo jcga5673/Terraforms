0bfbbaf1e5ea7f1315a0e281eb28f0e6501d3615
José Gallardo <jcga567345@gmail.com>
2021-11-08 23:45:01 -0600

    hook 1.2

--- DAGS/PostgressHook.py
+++ DAGS/PostgressHook.py
@@ -26,8 +26,18 @@ dag = DAG('insert_data_postgres',
           default_args=default_args,
           schedule_interval='@once',
           catchup=False)
-
+'''
 def csv_to_postgres(url):
+    try:
+        conection = pg.connect(
+            host = "postgres",
+            user = "airflow",
+            password = "airflow",
+            database = "airflow"
+        )
+        print('Conexión exitosa')
+    except Exception as err:
+        print(err,'no conection here brah')
     #Open Postgres Connection
     pg_hook = PostgresHook(postgres_conn_id='conn_postgress')
     get_postgres_conn = PostgresHook(postgres_conn_id='conn_postgress').get_conn()
@@ -41,14 +51,41 @@ def csv_to_postgres(url):
     df.to_parquet('data_frame.csv')
     for i, row in df.iterrows():
         try:
-            curr.execute("INSERT INTO user_purchase (invoice_number,stock_code, detail,quantity,invoice_date,unit_price,customer_id,country) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]))
+            cur = conection.curr()
+            cur.execute("INSERT INTO user_purchase (invoice_number,stock_code, detail,quantity,invoice_date,unit_price,customer_id,country) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]))
             print(i)
         except Exception as err:
             print(err,'solve this bro')
         if i == 50:
             break
     return 'well done'
-
+'''
+def csv_to_postgres(url):
+    try:
+        conection = pg.connect(
+            host = "postgres",
+            user = "airflow",
+            password = "airflow",
+            database = "airflow"
+        )
+        print('Conexión exitosa')
+    except Exception as err:
+        print(err,'no conection here brah')
+    #df = pd.read_csv('data_frame.csv') 
+    url = "https://drive.google.com/file/d/1ysfUdLi7J8gW6GDA3cOAbr7Zc4ZLhxxD/view?usp=sharing"
+    path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
+    df = pd.read_csv(path)
+    #engine = sqlalchemy.create_engine('postgresql://airflow:airflow@localhost:/postgres')
+    cur = conection.cursor()
+    for i, row in df.iterrows():
+        try:
+            cur.execute("INSERT INTO user_purchase (invoice_number,stock_code, detail,quantity,invoice_date,unit_price,customer_id,country) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]))
+            print(i)
+        except Exception as err:
+            print(err,'solve this bro')
+        if i == 50:
+            break
+    return 'here is postgress: '
 
 
 task1 = PythonOperator(task_id='csv_to_s3',
