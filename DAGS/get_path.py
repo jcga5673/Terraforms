3476a2209eed1620e97ab1269cdd5105a0da7cb6
José Gallardo <jcga567345@gmail.com>
2021-11-16 23:44:50 -0600

    try this

--- DAGS/get_path.py
+++ DAGS/get_path.py
@@ -9,6 +9,7 @@ from datetime import datetime, timedelta
 import os
 import pandas as pd
 
+'''
 
 default_args = {
     'owner': 'José',
@@ -63,3 +64,26 @@ t1 = PythonOperator(
     python_callable = path_local,
     dag = dag,
 )
+'''
+
+from airflow import DAG
+from airflow.settings import AIRFLOW_HOME
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+dag = DAG(
+    dag_id='where_is_home',
+    start_date=datetime.now()
+)
+
+def command():
+
+    print("$AIRFLOW_HOME=", AIRFLOW_HOME)
+
+with dag:
+    po = PythonOperator(
+        task_id='print',
+        python_callable=command,
+        provide_context=True
+    )
+
