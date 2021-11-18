9f4bd57b126de7bd583120e2db22daa0705bfb64
José Gallardo <jcga567345@gmail.com>
2021-11-17 23:44:03 -0600

    another day, another bread

---
+++ pyspark_codes/positive_reviews.py
@@ -0,0 +1,26 @@
+import pyspark
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+from pyspark.ml.feature import StopWordsRemover, Tokenizer, RegexTokenizer
+from pyspark.sql.functions import concat_ws,expr,col,array_contains
+
+
+spark = SparkSession.builder.appName('Basics').getOrCreate()
+df = spark.read.csv('movie_review.csv',header=True)
+###Tokenizer
+tokenizer = Tokenizer(outputCol="words_old")
+tokenizer.setInputCol("review_str")
+df = tokenizer.transform(df)
+#df1.show(1)
+### Stop remover
+remover = StopWordsRemover()
+remover.setInputCol("words_old")
+remover.setOutputCol("words")
+df = remover.transform(df)
+df = df.drop('words_old')
+#df1.show(1)
+df_sol = df.withColumn('positive', array_contains(df.words, 'good'))
+df_sol = df_sol.withColumn("positive_review", expr("CASE WHEN positive = 'true' THEN 1 " +
+               "ELSE 0 END"))
+df_sol = df_sol.drop('review_str','words','positive')
+#df_sol.show()
