2f65cdde5df01d3b3b8844cc7793539ebe4aeabb
José Gallardo <jcga567345@gmail.com>
2021-11-19 15:13:20 -0600

    here we are again

--- DAGS/emr_test.py
+++ DAGS/emr_test.py
@@ -182,7 +182,6 @@ terminate_emr_cluster = EmrTerminateJobFlowOperator(
     task_id="terminate_emr_cluster",
     job_flow_id="{{ task_instance.xcom_pull(task_ids='create_emr_cluster', key='return_value') }}",
     aws_conn_id="aws_default",
-    region_name = "us-east-2",
     dag=dag,
 )
 
