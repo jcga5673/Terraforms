246cf32897b6aa0d78db8d6c0f91b3efc8f1524f
José Gallardo <jcga567345@gmail.com>
2021-11-24 01:06:03 -0600

    terraform working

---
+++ DAGS/Final_project.py
@@ -0,0 +1,169 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.hooks.S3_hook import S3Hook
+from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.operators.emr_create_job_flow_operator import (
+    EmrCreateJobFlowOperator,
+)
+from airflow.contrib.operators.emr_add_steps_operator import EmrAddStepsOperator
+from airflow.contrib.sensors.emr_step_sensor import EmrStepSensor
+from airflow.contrib.operators.emr_terminate_job_flow_operator import (
+    EmrTerminateJobFlowOperator,
+)
+#from custom_modules.dag_s3_to_postgres import S3ToPostgresTransfer
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.hooks.S3_hook import S3Hook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.exceptions import AirflowException
+import os.path
+import pandas as pd
+import io
+
+
+
+# Configurations
+BUCKET_NAME = "data-bootcamp-jose"  # replace this with your bucket name
+#local_data = "./dags/data/movie_review.csv"
+s3_data_movie = "Data/movie_review.csv"
+s3_data_user= "Data/user_purchase.csv"
+#local_script = "./dags/scripts/spark/random_text_classification.py"
+s3_script = "final_pyspark_code.py"
+s3_clean = "clean_data/"
+
+SPARK_STEPS = [    
+    {
+        "Name": "Classify movie reviews",
+        "ActionOnFailure": "CANCEL_AND_WAIT",
+        "HadoopJarStep": {
+            "Jar": "command-runner.jar",
+            "Args": [
+                "spark-submit",
+                "--deploy-mode",
+                "client",
+                "s3://{{ params.BUCKET_NAME }}/{{ params.s3_script }}",
+            ],
+        },
+    }
+]    
+
+
+JOB_FLOW_OVERRIDES = {
+    "Name": "Movie review classifier",
+    "ReleaseLabel": "emr-5.29.0",
+    "Applications": [{"Name": "Hadoop"}, {"Name": "Spark"}],
+    "Configurations": [
+        {
+            "Classification": "spark-env",
+            "Configurations": [
+                {
+                    "Classification": "export",
+                    "Properties": {"PYSPARK_PYTHON": "/usr/bin/python3"},
+                }
+            ],
+        }
+    ],
+    "Instances": {
+        "InstanceGroups": [
+            {
+                "Name": "Master node",
+                "Market": "SPOT",
+                "InstanceRole": "MASTER",
+                "InstanceType": "m4.xlarge",
+                "InstanceCount": 1,
+            },
+            {
+                "Name": "Core - 2",
+                "Market": "SPOT",
+                "InstanceRole": "CORE",
+                "InstanceType": "m4.xlarge",
+                "InstanceCount": 2,
+            },
+        ],
+        "KeepJobFlowAliveWhenNoSteps": True,
+        "TerminationProtected": False,
+    },
+    "JobFlowRole": "EMR_EC2_DefaultRole",
+    "ServiceRole": "EMR_DefaultRole",
+}
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": True,
+    "wait_for_downstream": True,
+    "start_date": datetime(2020, 10, 17),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+dag = DAG(
+    "spark_submit_airflow",
+    default_args=default_args,
+    schedule_interval="0 10 * * *",
+    max_active_runs=1,
+)
+
+start_data_pipeline = DummyOperator(task_id="start_data_pipeline", dag=dag)
+
+# Create an EMR cluster
+create_emr_cluster = EmrCreateJobFlowOperator(
+    task_id="create_emr_cluster",
+    job_flow_overrides=JOB_FLOW_OVERRIDES,
+    aws_conn_id="aws_default",
+    region_name = "us-east-2",
+    dag=dag,
+)
+
+# Add your steps to the EMR cluster
+step_adder = EmrAddStepsOperator(
+    task_id="add_steps",
+    job_flow_id="{{ task_instance.xcom_pull(task_ids='create_emr_cluster', key='return_value') }}",
+    aws_conn_id="aws_default",
+    steps=SPARK_STEPS,
+    params={
+        "BUCKET_NAME": BUCKET_NAME,
+        "s3_data_movie": s3_data_movie,
+        "s3_data_user": s3_data_user,
+        "s3_script": s3_script,
+        #"s3_clean": s3_clean,
+    },
+    dag=dag,
+)
+
+last_step = len(SPARK_STEPS) - 1
+# wait for the steps to complete
+step_checker = EmrStepSensor(
+    task_id="watch_step",
+    job_flow_id="{{ task_instance.xcom_pull('create_emr_cluster', key='return_value') }}",
+    step_id="{{ task_instance.xcom_pull(task_ids='add_steps', key='return_value')["
+    + str(last_step)
+    + "] }}",
+    aws_conn_id="aws_default",
+    dag=dag,
+)
+
+# Terminate the EMR cluster
+terminate_emr_cluster = EmrTerminateJobFlowOperator(
+    task_id="terminate_emr_cluster",
+    job_flow_id="{{ task_instance.xcom_pull(task_ids='create_emr_cluster', key='return_value') }}",
+    aws_conn_id="aws_default",
+    dag=dag,
+)
+
+
+
+
+
+
+end_data_pipeline = DummyOperator(task_id="end_data_pipeline", dag=dag)
+
+
+start_data_pipeline >>  create_emr_cluster
+create_emr_cluster >> step_adder >> step_checker >> terminate_emr_cluster
+terminate_emr_cluster >> end_data_pipeline
---
+++ pyspark_codes/final_pyspark_code.py
@@ -0,0 +1,49 @@
+import pyspark
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+from pyspark.ml.feature import StopWordsRemover, Tokenizer, RegexTokenizer
+from pyspark.sql.functions import concat_ws,expr,col,array_contains,format_number,regexp_replace
+from pyspark.sql.types import DoubleType,LongType
+
+
+spark = SparkSession.builder.appName('Final_code').getOrCreate()
+
+### df_movies cleaning
+df_movies = spark.read.csv('s3://data-bootcamp-jose/Data/movie_review.csv',header=True)
+
+tokenizer = Tokenizer(outputCol="words_old")
+tokenizer.setInputCol("review_str")
+df_movies = tokenizer.transform(df_movies)
+
+remover = StopWordsRemover()
+remover.setInputCol("words_old")
+remover.setOutputCol("words")
+
+df_movies = remover.transform(df_movies)
+df_movies = df_movies.drop('words_old')
+df_movies = df_movies.withColumn('positive', array_contains(df_movies.words, 'good'))
+df_movies = df_movies.withColumn("positive_review", expr("CASE WHEN positive = 'true' THEN 1 " +
+               "ELSE 0 END"))
+df_movies = df_movies.drop('review_str','words','positive')
+
+df_movies = df_movies.groupby('cid').agg(f.count('positive_review').alias('review_count'),f.sum('positive_review').alias('review_score'))
+
+### df_user cleaning
+
+df_user = spark.read.csv('s3://data-bootcamp-jose/Data/user_purchase.csv',header=True)
+
+df_user = df_user.withColumn("Quantity", regexp_replace(df_user.Quantity, r"\W", ""))
+df_user = df_user.na.fill(0)
+df_user = df_user.withColumn("Quantity",df_user.Quantity.cast(LongType()))
+df_user = df_user.withColumn("UnitPrice",df_user.UnitPrice.cast(LongType()))
+df_user = df_user.withColumn('amount_spent', df_user['Quantity'] * df_user['UnitPrice'])
+
+df_user = df_user.groupBy("CustomerID").agg(f.sum(df_user.amount_spent).alias('sum'))
+df_user = df_user.withColumn("amount_spent", format_number(df_user.sum, 2)).drop('sum')
+df_user = df_user.withColumn("amount_spent", regexp_replace(df_user.amount_spent, ",", ""))
+df_user = df_user.withColumn("amount_spent",df_user.amount_spent.cast(LongType()))
+
+df_joined = df_user.join(df_movies,df_movies.cid == df_user.CustomerID).drop('cid')
+df_joined = df_joined.withColumn("CustomerID",df_joined.CustomerID.cast(LongType()))
+
+df_joined.repartition(1).write.format('csv').option('header','true').save('s3a://data-bootcamp-jose/final_result',mode='overwrite')
