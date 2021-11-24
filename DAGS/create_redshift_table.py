eb8ca18fdf8549cf95e8c33ec0f386b19a387b9d
José Gallardo <jcga567345@gmail.com>
2021-11-24 01:22:29 -0600

    3 more days

--- DAGS/create_redshift_table.py
+++ DAGS/create_redshift_table.py
@@ -1,12 +1,6 @@
-from os import getenv
-
+from datetime import datetime
 from airflow import DAG
-from airflow.decorators import task
-from airflow.models.baseoperator import chain
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.amazon.aws.operators.redshift import RedshiftSQLOperator
-from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
-from airflow.utils.dates import days_ago
 
 
 
@@ -20,6 +14,6 @@ dag_params = {
 with DAG(**dag_params) as dag:
 
     setup__task_create_table = RedshiftSQLOperator(
-        sql=f'CREATE TABLE IF NOT EXISTS user_behavior_metric(costumerid int, amount_spent int,review_score int, review_count int)',
+        sql='CREATE TABLE IF NOT EXISTS user_behavior_metric(costumerid int, amount_spent int,review_score int, review_count int)',
         task_id='create_table',
     )
