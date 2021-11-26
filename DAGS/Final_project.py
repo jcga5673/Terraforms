6e3e4d2bdb96a4ab9bfd8f3c9178e88c136ded34
José Gallardo <jcga567345@gmail.com>
2021-11-26 17:41:04 -0600

    here

--- DAGS/Final_project.py
+++ DAGS/Final_project.py
@@ -190,7 +190,7 @@ check_code = PythonOperator(
 
 transfer_s3_to_redshift = S3ToRedshiftOperator(
     s3_bucket='data-bootcamp-jose',
-    s3_key="Test_result.csv",
+    s3_key="{{ task_instance.xcom_pull(task_ids='list_objects') }}",
     schema="public",
     table="user_behavior_metric",
     copy_options=['csv'],
