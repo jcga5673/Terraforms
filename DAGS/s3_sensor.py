f544c6b60950fac685e6ad5691410343d3ae808e
José Gallardo <jcga567345@gmail.com>
2021-12-09 15:19:03 -0600

    final project

--- DAGS/s3_sensor.py
+++ DAGS/s3_sensor.py
@@ -3,7 +3,7 @@ S3 Sensor Connection Test
 """
 
 from airflow import DAG
-from airflow.operators import HttpSensor,   BashOperator, EmailOperator, S3KeySensor
+from airflow.operators import   BashOperator,  S3KeySensor
 from datetime import datetime, timedelta
 
 default_args = {
